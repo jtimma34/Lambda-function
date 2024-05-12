268e4f08168db1c945fbf76be924679212ab7382
jtimma34 <133996358+jtimma34@users.noreply.github.com>
2024-05-12 14:25:12 -0400

    Delete lambda_fucntion.py

--- lambda_fucntion.py
+++
@@ -1,9 +0,0 @@
-import json
-import datetime
-
-def lambda_handler(event, context):
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"currentTime": current_time})
-    }
